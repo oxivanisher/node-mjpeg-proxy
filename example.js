9ccb278fb5d7fbab0f78bd291afd0fdcaafb004f
Marc Urben <aegnor@mittelerde.ch>
2019-09-23 22:27:11 +0200

    fixing example code

--- example.js
+++ example.js
@@ -2,5 +2,5 @@ var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
 var app = express();
 
-app.get('/index1.jpg', new MjpegProxy({mjpegUrl: 'http://admin:admin@192.168.1.109/cgi/mjpg/mjpg.cgi'}).proxyRequest);
+app.get('/index1.jpg', new MjpegProxy('http://admin:admin@192.168.1.109/cgi/mjpg/mjpg.cgi').proxyRequest);
 app.listen(8080);
